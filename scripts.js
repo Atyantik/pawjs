27c1ebb79aecac7ed324a5bfa1a9e0bacdd60ed7
Tirth Bodawala <tirthbodawala@atyantik.com>
2017-12-18 19:05:48 +0530

    Add docker support

--- scripts.js
+++ scripts.js
@@ -22,51 +22,61 @@ if (allCommands.length) {
   userCommand = _.first(allCommands);
 }
 
-const NODE_ENV = process.env.NODE_ENV || "development";
-
-// Set NODE_ENV if not already set
-process.env.NODE_ENV = NODE_ENV;
-
 // Disable babel cache
 process.env.BABEL_DISABLE_CACHE = 1;
 
 let allExecutablePaths = process.env.PATH.split(path.delimiter);
-allExecutablePaths.unshift(__dirname);
-allExecutablePaths.unshift(`${__dirname}${path.sep}.bin`);
-allExecutablePaths.unshift(`${__dirname}${path.sep}node_modules${path.sep}.bin`);
 
+// Add current folder to executable path
+allExecutablePaths.unshift(__dirname);
+
+// Include current folder bin and node_modules's bin
+allExecutablePaths.unshift(path.join(__dirname, ".bin"));
+allExecutablePaths.unshift(path.join(__dirname, "node_modules", ".bin"));
+
+// Find command from all the paths possible
 function findCommandPath(com) {
   let execPath = "";
+  const possibleExtension = ["", ".exe", ".cmd"];
   _.each(allExecutablePaths, executablePath => {
     if (execPath.length) return;
 
-    if (fs.existsSync(`${executablePath}${path.sep}${com}`)) {
-      return execPath = `${executablePath}${path.sep}${com}`;
-    }
-    if (fs.existsSync(`${executablePath}${path.sep}${com}.exe`)) {
-      return execPath = `${executablePath}${path.sep}${com}.exe`;
-    }
-    if (fs.existsSync(`${executablePath}${path.sep}${com}.cmd`)) {
-      return execPath = `${executablePath}${path.sep}${com}.cmd`;
-    }
+    _.each(possibleExtension, ext => {
+      if (execPath.length) return;
+
+      const extendedPath = path.join(executablePath, `${com}${ext}`);
+      if (fs.existsSync(extendedPath)) {
+        execPath = extendedPath;
+      }
+    });
   });
-  return execPath.length ? execPath: undefined;
+  if (!execPath.length) throw new Error(`Cannot find command ${com}.`);
+  return execPath;
 }
 
 
 const lint = () => {
+  if (!path.resolve(`${process.env.__p_root}.eslintrc`)) {
+    // eslint-disable-next-line
+    console.log("Cannot find .eslintrc in root folder! Thus nothing to lint...");
+    return { code: 0, stdout: "", };
+  }
   return shell.exec(`${findCommandPath("eslint")} -c ${process.env.__p_root}.eslintrc --ignore-path ${process.env.__p_root}.gitignore ${process.env.__p_root}src`);
+};
+
+const test = () => {
+  return shell.exec(`${findCommandPath("cross-env")} NODE_ENV=test ${findCommandPath("mocha")} --require babel-core/register "${process.env.__p_root}src/**/*.test.js"`);
 };
 switch(userCommand) {
   case "start": {
     process.env.NODE_ENV = process.env.NODE_ENV || "development";
     nodemon({
-      script: `${currentDir}${path.sep}src${path.sep}server.js`,
+      script: path.join(currentDir, "src", "server.js"),
       execMap: {
         "js": findCommandPath("babel-node")
       },
       "watch": [
-        `${currentDir}${path.sep}src${path.sep}server.js`
+        path.join(currentDir, "src", "server.js"),
       ]
     });
     break;
@@ -115,11 +125,59 @@ switch(userCommand) {
     return process.exit(result.code);
   }
   case "test": {
+    // try lint and then complete the test
     const lintResults = lint();
     if (lintResults.code) {
       return process.exit(lintResults.code);
     }
-    const testResults = shell.exec(`${findCommandPath("cross-env")} NODE_ENV=test ${findCommandPath("mocha")} --require babel-core/register "${process.env.__p_root}src/**/*.test.js"`);
+    const testResults = test();
     return process.exit(testResults.code);
+  }
+  case "docker": {
+    process.env.NODE_ENV = process.env.NODE_ENV || "development";
+    if(!shell.which("docker")) {
+      // eslint-disable-next-line
+      console.log("command: 'docker' not found. Please install docker and try again. https://www.docker.com/");
+      return process.exit(1);
+    }
+
+    // If .dockerignore does not exists in project root, then create it
+    if (!fs.existsSync(path.join(process.env.__p_root, ".dockerignore"))) {
+      // eslint-disable-next-line
+      console.log("Creating .dockerignore file as id does not exists in project root");
+      shell.cp("-R", path.join(process.env.__c_root, ".dockerignore"), process.env.__p_root);
+    }
+
+    let otherArgs = Array.from(process.argv).slice(3);
+
+    const hasDockerFileInProjectRoot = fs.existsSync(path.join(process.env.__p_root, "Dockerfile"));
+
+    if (
+      !hasDockerFileInProjectRoot &&
+      _.first(otherArgs) &&
+      _.first(otherArgs).toLowerCase() === "build" &&
+      _.indexOf(otherArgs, "-f") === -1 &&
+      _.indexOf(otherArgs, "-F") === -1
+    ) {
+      // eslint-disable-next-line
+      console.log("No docker file found in project root and no option for docker file specified!");
+
+      const devDockerfilePath = path.join(process.env.__p_root, "docker", "Dockerfile");
+      const prodDockerfilePath = path.join(process.env.__p_root, "docker", "prod", "Dockerfile");
+
+      if (
+        (process.env.NODE_ENV.toLowerCase() === "production" && !fs.existsSync(prodDockerfilePath)) ||
+        (process.env.NODE_ENV.toLowerCase() === "development" && !fs.existsSync(devDockerfilePath))
+      ) {
+        // eslint-disable-next-line
+        console.log("No Dockerfile found! Thus creating new in folder 'docker'");
+        shell.cp("-R", path.join(process.env.__c_root, "src", "docker"), `${process.env.__p_root}`);
+      }
+      const dockerImagePath = process.env.NODE_ENV.toLowerCase() === "production"? prodDockerfilePath : devDockerfilePath;
+      otherArgs = ["build", "-f", dockerImagePath, ...otherArgs.slice(1)];
+    }
+    // eslint-disable-next-line
+    console.log(shell.exec(["docker", ...otherArgs].join(" ")).stdout);
+    break;
   }
 }
