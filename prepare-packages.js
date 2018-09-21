d761b2433ea8e986d46f281d298c548dd7980e9d
Tirth Bodawala <tirthbodawala@atyantik.com>
2018-09-21 12:09:56 +0530

    Update package.json of packages and demo project for smooth installation

---
+++ prepare-packages.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const resolve = require("path").resolve;
+const join = require("path").join;
+const cp = require("child_process");
+const os = require("os");
+
+// get packages path
+const lib = resolve(__dirname, "./packages/");
+
+fs.readdirSync(lib)
+  .forEach(function (mod) {
+    const modPath = join(lib, mod);
+    // ensure path has package.json
+    if (!fs.existsSync(join(modPath, "package.json"))) return;
+
+    // npm binary based on OS
+    const npmCmd = os.platform().startsWith("win") ? "npm.cmd" : "npm";
+
+    // Install folder
+    cp.spawnSync(npmCmd, ["i"], { env: process.env, cwd: modPath, stdio: "inherit" });
+    cp.spawnSync(npmCmd, ["run" , "build"], { env: process.env, cwd: modPath, stdio: "inherit" });
+  });
