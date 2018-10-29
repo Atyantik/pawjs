0ad48815bb35de49f91f249eafdc113e3abfd53a
Tirth Bodawala <tirthbodawala@atyantik.com>
2018-10-29 17:56:09 +0530

    Re-enable prefetch-loader within src folder of project only

--- src/webpack/loaders/prefetch-loader.js
+++ src/webpack/loaders/prefetch-loader.js
@@ -1,3 +1,8 @@
+const directories = require('../utils/directories');
+
 module.exports = function prefetchLoader(source) {
-  return source.replace(/import\([^/\\*](.*)("|')\)/g, 'import(/* webpackPrefetch: true */"$1")');
+  if (this.resourcePath.indexOf(directories.src) !== -1) {
+    return source.replace(/import\([^/\\*](.*)("|')\)/g, 'import(/* webpackPrefetch: true */"$1")');
+  }
+  return source;
 };
