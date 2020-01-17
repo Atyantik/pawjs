814576b2595232812f3d4be479578f25c6670dfe
vishal <vishalgheravada@atyantik.com>
2020-01-17 16:45:38 +0530

    Shows better spawn output

--- src/scripts/cli.ts
+++ src/scripts/cli.ts
@@ -324,7 +324,8 @@ export default class CliHandler {
       {
         env,
         shell: true,
-        stdio: [process.stdin, process.stdout, 'pipe'],
+        detached: true,
+        stdio: 'inherit',
       },
     );
 
@@ -357,7 +358,8 @@ export default class CliHandler {
           {
             env,
             shell: true,
-            stdio: [process.stdin, process.stdout, 'pipe'],
+            detached: true,
+            stdio: 'inherit',
           },
         );
       }
@@ -373,30 +375,31 @@ export default class CliHandler {
     const env = Object.create(process.env);
     env.NODE_ENV = 'test';
 
-    let tscRoot = this.libRoot;
-    if (fs.existsSync(path.join(this.projectRoot, 'tsconfig.json'))) {
-      tscRoot = this.projectRoot;
-    }
-    const tsc = spawn(this.searchCommand('tsc'), ['-p', tscRoot], {
-      env,
-      shell: true,
-      stdio: [process.stdin, process.stdout, 'pipe'],
-    });
+    // let tscRoot = this.libRoot;
+    // if (fs.existsSync(path.join(this.projectRoot, 'tsconfig.json'))) {
+    //   tscRoot = this.projectRoot;
+    // }
+    // const tsc = spawn(this.searchCommand('tsc'), ['-p', tscRoot], {
+    //   env,
+    //   shell: true,
+    //   detached: true,
+    //   stdio: 'inherit',
+    // });
 
-    tsc.on('close', (errorCode) => {
-      if (!this.searchCommand) {
-        // eslint-disable-next-line no-console
-        console.log('Application not configured properly, cannot search for commands');
-        return;
-      }
-      if (!errorCode) {
-        spawn(this.searchCommand('jest'), ['--verbose'], {
-          env,
-          shell: true,
-          stdio: [process.stdin, process.stdout, 'pipe'],
-        });
-      }
-    });
+    // tsc.on('close', (errorCode) => {
+    //   if (!this.searchCommand) {
+    //     // eslint-disable-next-line no-console
+    //     console.log('Application not configured properly, cannot search for commands');
+    //     return;
+    //   }
+    //   if (!errorCode) {
+    //     spawn(this.searchCommand('jest'), ['--verbose'], {
+    //       env,
+    //       shell: true,
+    //       stdio: [process.stdin, process.stdout, 'pipe'],
+    //     });
+    //   }
+    // });
   }
 
   run() {
