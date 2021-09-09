db908387b8e0a0f574bbdefb79b075c34d3b0c9e
Tirth Bodawala <tirthbodawala@atyantik.com>
2021-09-09 15:28:17 +0530

    Correct package mapping irrespective of @types

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -1,2 +1,7 @@
 declare function getDefault(req: any): any;
 declare function pawExistsSync(filePath: string, fileSystem?: any): string;
+
+declare module 'webpack';
+declare module 'webpack-dev-middleware';
+declare module 'webpack-hot-middleware';
+declare module 'mini-css-extract-plugin';
