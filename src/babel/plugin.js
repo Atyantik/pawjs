168694cb7e171d49b32eb59191758d75a86fea47
Tirth Bodawala <tirthbodawala@atyantik.com>
2021-10-30 02:11:09 +0530

    Add dynamic import webpack back to make sure the CSS is loaded as expected

--- src/babel/plugin.js
+++ src/babel/plugin.js
@@ -1,7 +1,9 @@
 /* global getDefault */
 const reactHotLoader = getDefault(require('react-hot-loader/babel'));
 const lodash = getDefault(require('babel-plugin-lodash'));
+const syntaxDynamicImport = getDefault(require('@babel/plugin-syntax-dynamic-import'));
 const reactLoadableRoutes = getDefault(require('./plugins/react-loadable-routes'));
+const dynamicImportWebpack = getDefault(require('./plugins/dynamic-import-webpack'));
 
 const pluginDefaults = {
   noChunk: false,
@@ -12,6 +14,7 @@ module.exports = (options) => {
   const o = { ...pluginDefaults, ...options };
   return [
     reactLoadableRoutes,
+    o.noChunk ? dynamicImportWebpack : syntaxDynamicImport,
     lodash,
     ...(o.hot ? [reactHotLoader] : []),
   ];
