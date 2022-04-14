aff447d2447d0e8d62a0fa409d00f31451357ca0
Tirth Bodawala <tirthbodawala@atyantik.com>
2022-04-14 16:07:52 +0530

    No data url conversion in images

--- src/webpack/inc/babel-image-rule.ts
+++ src/webpack/inc/babel-image-rule.ts
@@ -3,11 +3,12 @@ import { extensionRegex, imageAssetsExtensions } from '../utils/assets';
 export default () => ({
   test: extensionRegex(imageAssetsExtensions),
   type: 'asset',
-  parser: {
-    dataUrlCondition: {
-      maxSize: 4 * 1024, // 4kb
-    },
-  },
+  // Not doing dataURL condition as it is causing issues with SVG #use in safari
+  // parser: {
+  //   dataUrlCondition: {
+  //     maxSize: 4 * 1024, // 4kb
+  //   },
+  // },
   generator: {
     // We will use content hash for long term caching of asset
     filename: 'images/[contenthash]-[name][ext][query]',
