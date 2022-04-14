bc140c26abd13a2220027b19c495fd134a270c8d
Tirth Bodawala <tirthbodawala@atyantik.com>
2022-04-14 19:17:24 +0530

    Do not convert images to dataurl

--- src/webpack/inc/babel-image-rule.ts
+++ src/webpack/inc/babel-image-rule.ts
@@ -4,11 +4,11 @@ export default () => ({
   test: extensionRegex(imageAssetsExtensions),
   type: 'asset',
   // Not doing dataURL condition as it is causing issues with SVG #use in safari
-  // parser: {
-  //   dataUrlCondition: {
-  //     maxSize: 4 * 1024, // 4kb
-  //   },
-  // },
+  parser: {
+    dataUrlCondition: {
+      maxSize: 0, // You can make it 4kb by 4 * 1024
+    },
+  },
   generator: {
     // We will use content hash for long term caching of asset
     filename: 'images/[contenthash]-[name][ext][query]',
